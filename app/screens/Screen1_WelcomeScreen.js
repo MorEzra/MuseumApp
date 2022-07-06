7209f3a32100e3228b41dc306535a08cd8d8ecc4
YonatanOfek <38861891+YonatanOfek@users.noreply.github.com>
2022-07-06 22:36:53 +0300

    fix typo

--- app/screens/Screen1_WelcomeScreen.js
+++ app/screens/Screen1_WelcomeScreen.js
@@ -34,7 +34,7 @@ export default function WelcomeScreen({navigation}) {
           onPress={() => {
             let finishingTime = performance.now();
             welcomeScreenTotalTime = (finishingTime - startingTime);
-            navigation.navigate("OverviewScreen");
+            navigation.navigate("Questionnaire");
            }
           }>
       </Button>
