05a60bb33b56f12b7bb90ced4d35ccbbf139ab15
Johnny <antonykoafe@gmail.com>
2022-07-23 16:00:14 +0300

    Added a "read the rules" checkbox, fixed text

--- app/screens/Screen1_WelcomeScreen.js
+++ app/screens/Screen1_WelcomeScreen.js
@@ -2,32 +2,47 @@ import React  from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { globalStyles } from '../assets/styles/global';
+import { RadioButton } from 'react-native-paper';
 
 export let welcomeScreenTotalTime;
 export let active = Math.round(Math.random())
 export default function WelcomeScreen({navigation}) {
   let startingTime = performance.now();
+  const [checked, setChecked] = React.useState('unchecked');
   
   return (      
     <View style={globalStyles.container}>
      
-      <Text style = {globalStyles.header}>ניסוי העדפות במוזיאון תל אביב</Text>
+      <Text style = {globalStyles.header}>ביקור באוסף מינזה בלומנטל במוזיאון תל אביב</Text>
 
       <Text style={globalStyles.instructionsText}>
       תודה על השתתפותך בסיור שהינו חלק ממחקר מטעם אוניברסיטת תל אביב.{"\n"}
 
-במהלך הניסוי תתבקש לעבור בין יצירות שונות מאוסף המוזיאון ולשמוע הסברים מוקלטים אודותן מאת ד"ר ספי הנדלר מוועדת התערוכות של המוזיאון.
+במהלך הסיור תתבקש לעבור בין יצירות שונות מאוסף המוזיאון ולשמוע הסברים מוקלטים אודותן מאת ד"ר ספי הנדלר מוועדת התערוכות של המוזיאון.
 {"\n"}לפני ולאחר הסיור תתבקש לענות על מספר שאלות.{"\n"}
 
-משך הניסוי כ-20 דקות ובתמורה להשתתפותך תזכה בשובר.{"\n"}
+משך הסיור כ-20 דקות ובתמורה להשתתפותך תזכה בשובר.{"\n"}
 
 הינך חופשי לבחור שלא להשתתף במחקר ולהפסיק את השתתפותך בכל עת.{"\n"} 
-כלל הנתונים מהניסוי הינם חסויים ומשמשים רק לצורך המחקר. 
+כלל הנתונים מהשתתפותך הינם חסויים ומשמשים רק לצורך המחקר. 
 
-הניסוי מנוסח בלשון זכר אך מיועד לשני המינים.{"\n"}{"\n"}
-בלחיצה על 'המשך' הינך מסכים להשתתף בניסוי, תהנו!
+ההסברים וההוראות מנוסחות בלשון זכר אך מיועדות לשני המינים.{"\n"}{"\n"}
+בלחיצה על 'המשך' הינך מסכים להשתתף במחקר, תהנו!
 
       </Text>
+      
+      <RadioButton.Item
+        value="unchecked"
+        label="קראתי את ההוראות ואני מאשר את השתתפותי בניסוי"
+        status={ checked === 'checked' ? 'checked' : 'unchecked' }
+        onPress={() => {
+          if (checked === 'unchecked')
+           {setChecked('checked')}
+          else 
+           { setChecked('unchecked')}
+          }
+        }
+      />
 
       <Button           
           title="המשך"
--- app/screens/Screen3_ResearchGuidelines.js
+++ app/screens/Screen3_ResearchGuidelines.js
@@ -9,11 +9,11 @@ export default function ResearchGuidelines({navigation}) {
     let startingTime = performance.now();
     return (      
       <View style={globalStyles.container}>                
-        <Text style = {globalStyles.header} >שלבי הניסוי</Text>
+        <Text style = {globalStyles.header} >מהלך הסיור</Text>
         <View>
-          <Text style={globalStyles.instructionsText}>תוצג עבורך התמונה ההתחלתית ממנה מתחיל הסיור, תתבקש לצלם את התמונה בהגיעך אליה ולשמוע קטע קול קצר אודותיה. </Text>
-          <Text style={globalStyles.instructionsText}>לאחר מכן, על מנת להתאים לך את התמונה בצורה הטובה ביותר, עליך לבחור מתוך מספר אפשרויות מה תרצה לראות בתמונה הבאה. </Text>
-          <Text style={globalStyles.instructionsText}>בצורה זו תמשיך לעבור בין 8 תמונות במוזיאון.</Text>
+          <Text style={globalStyles.instructionsText}>תוצג עבורך התמונה ההתחלתית ממנה מתחיל הסיור, תתבקש לצלם את התמונה בהגיעך אליה ולשמוע הסבר קצר אודותיה. </Text>
+          <Text style={globalStyles.instructionsText}>לאחר מכן, על מנת להתאים לך את התמונה הבאה בצורה הטובה ביותר, עליך לבחור מתוך מספר אפשרויות מה תרצה לראות בהמשך. </Text>
+          <Text style={globalStyles.instructionsText}>בצורה זו תעבור בין 8 יצירות באוסף.</Text>
           <Text style={globalStyles.instructionsText}>לבסוף, תתבקש לענות על מספר שאלות אודות הסיור והיצירות.</Text>                
         </View>
         <Button 
