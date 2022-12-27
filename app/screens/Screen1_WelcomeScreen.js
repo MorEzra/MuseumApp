96fa12fe4c861da24b1a0d9d5d8dafa01bf5e0ff
Mor Ezra <mor3311@gmail.com>
2022-12-27 16:54:37 +0200

    added setActive function (in order to set active by examineeNum) and added activePassiveCombined option (depends on examineeNum)

--- app/screens/Screen1_WelcomeScreen.js
+++ app/screens/Screen1_WelcomeScreen.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { globalStyles } from '../assets/styles/global';
@@ -7,76 +7,87 @@ import { RadioButton } from 'react-native-paper';
 
 export let tExperimentBegin = performance.now(); // TODO: remove
 export let tFinishWelcomeScreen;
-export let active = 0;
+
+let active = 0;
+let activePassiveCombined = 0;
+
+function setActive(value) {
+  active = value;
+}
+
+function setActivePassiveCombined(value) {
+  activePassiveCombined = value;
+}
+
+export { active, activePassiveCombined, setActive, setActivePassiveCombined };
+
 export let debugMode = 1;
-export let noPictureMode= false;
+export let noPictureMode = false;
 export let sessionTimer = new Date();
 
-export default function WelcomeScreen({navigation}) {
+export default function WelcomeScreen({ navigation }) {
   const [checked, setChecked] = React.useState('unchecked');
-  
-  return (      
-    <View style={globalStyles.container}>
-      <Text style = {globalStyles.header}>ביקור באוסף מינזה בלומנטל במוזיאון תל אביב</Text>
 
-        <Text style={globalStyles.instructionsText}>
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.header}>ביקור באוסף מינזה בלומנטל במוזיאון תל אביב</Text>
+
+      <Text style={globalStyles.instructionsText}>
         תודה על השתתפותכם בסיור שהינו חלק ממחקר מטעם אוניברסיטת תל אביב.{"\n"}
 
-  במהלך הסיור תתבקשו לעבור בין יצירות שונות מאוסף המוזיאון ולשמוע הסברים מוקלטים אודותן מאת ד"ר ספי הנדלר מוועדת התערוכות של המוזיאון.
-  {"\n"}לפני ולאחר הסיור תתבקשו לענות על מספר שאלות.{"\n"}
+        במהלך הסיור תתבקשו לעבור בין יצירות שונות מאוסף המוזיאון ולשמוע הסברים מוקלטים אודותן מאת ד"ר ספי הנדלר מוועדת התערוכות של המוזיאון.
+        {"\n"}לפני ולאחר הסיור תתבקשו לענות על מספר שאלות.{"\n"}
 
-  משך הסיור כ-20 דקות ובתמורה להשתתפותך תזכה בשובר.{"\n"}
+        משך הסיור כ-20 דקות ובתמורה להשתתפותך תזכה בשובר.{"\n"}
 
-  הינכם חופשיים לבחור שלא להשתתף במחקר ולהפסיק את השתתפותכם בכל עת.{"\n"} 
-   כלל הנתונים מהשתתפותכם הינם חסויים ומשמשים לצורך מחקר בלבד. 
+        הינכם חופשיים לבחור שלא להשתתף במחקר ולהפסיק את השתתפותכם בכל עת.{"\n"}
+        כלל הנתונים מהשתתפותכם הינם חסויים ומשמשים לצורך מחקר בלבד.
 
-    {"\n"}{"\n"}
-  בלחיצה על 'המשך' הינכם מסכימים להשתתף במחקר, תהנו!
+        {"\n"}{"\n"}
+        בלחיצה על 'המשך' הינכם מסכימים להשתתף במחקר, תהנו!
 
-        </Text>
-        
-        <View style = {{flexDirection:"row", justifyContent:"space-between"}}>
-          <Image        
-                source={require("../assets/images/logos/museum.png")}
-                style={{ resizeMode: 'contain', width: 150, height: 150, marginBottom:20, marginLeft:25, marginRight:75}} 
-          />
-          <Image        
-                source={require("../assets/images/logos/felizia.png")}
-                style={{ resizeMode: 'contain', width: 150, height: 150, marginBottom:20}} 
-          />
-          <Image        
-                source={require("../assets/images/logos/tau.png")}
-                style={{ resizeMode: 'contain', width: 150, height: 150, marginBottom:20, marginRight:25, marginLeft:75}} 
-          />
-        </View>
-        
-        <RadioButton.Item
-          value="unchecked"
-          label="קראתי את ההוראות ואני מאשר את השתתפותי בניסוי"
-          status={ checked === 'checked' ? 'checked' : 'unchecked' }
-          onPress={() => {
-            if (checked === 'unchecked')
-            {setChecked('checked')}
-            else 
-            { setChecked('unchecked')}
-            }
-          }
+      </Text>
+
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Image
+          source={require("../assets/images/logos/museum.png")}
+          style={{ resizeMode: 'contain', width: 150, height: 150, marginBottom: 20, marginLeft: 25, marginRight: 75 }}
         />
-      <View style = {globalStyles.buttonView}>
+        <Image
+          source={require("../assets/images/logos/felizia.png")}
+          style={{ resizeMode: 'contain', width: 150, height: 150, marginBottom: 20 }}
+        />
+        <Image
+          source={require("../assets/images/logos/tau.png")}
+          style={{ resizeMode: 'contain', width: 150, height: 150, marginBottom: 20, marginRight: 25, marginLeft: 75 }}
+        />
+      </View>
+
+      <RadioButton.Item
+        value="unchecked"
+        label="קראתי את ההוראות ואני מאשר את השתתפותי בניסוי"
+        status={checked === 'checked' ? 'checked' : 'unchecked'}
+        onPress={() => {
+          if (checked === 'unchecked') { setChecked('checked') }
+          else { setChecked('unchecked') }
+        }
+        }
+      />
+      <View style={globalStyles.buttonView}>
         <TouchableOpacity
-            style = {globalStyles.button}        
-            disabled = { checked === 'checked' || debugMode ? false : true }
-            onPress={() => {        
-              let tFinishWelcomeScreenDate = new Date();
-              tFinishWelcomeScreen = tFinishWelcomeScreenDate.getHours() + ":" + tFinishWelcomeScreenDate.getMinutes() + ":" + tFinishWelcomeScreenDate.getSeconds() + ":" + tFinishWelcomeScreenDate.getMilliseconds();
-              navigation.navigate("Questionnaire");
-            }
-            }>
-              <Text style = {globalStyles.buttonText}>המשך</Text>
+          style={globalStyles.button}
+          disabled={checked === 'checked' || debugMode ? false : true}
+          onPress={() => {
+            let tFinishWelcomeScreenDate = new Date();
+            tFinishWelcomeScreen = tFinishWelcomeScreenDate.getHours() + ":" + tFinishWelcomeScreenDate.getMinutes() + ":" + tFinishWelcomeScreenDate.getSeconds() + ":" + tFinishWelcomeScreenDate.getMilliseconds();
+            navigation.navigate("Questionnaire");
+          }
+          }>
+          <Text style={globalStyles.buttonText}>המשך</Text>
         </TouchableOpacity>
-      </View>        
-        <StatusBar style="auto" />
-     
+      </View>
+      <StatusBar style="auto" />
+
     </View>
   );
 }
